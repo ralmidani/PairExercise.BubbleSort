9064fe065eb81c7ac715ffe140e037d320e6e68d
Blake Zeiger <blakezeiger@gmail.com>
2018-03-05 12:57:42 -0500

    updates

--- bubblesort.js
+++ bubblesort.js
@@ -1,23 +1,39 @@
-function bubbleSort(arr) {
+
+
+
+function bubbleSort(arr, func) {
   let didSwap = true;
   let unsortedEnd = arr.length - 1;
   while (unsortedEnd !== 0 && didSwap === true) {
     didSwap = false;
+
     for (let i = 0; i < unsortedEnd; i++) {
-      if (arr[i] > arr[i + 1]) {
+      if (func) {
+        if(func(arr[i], arr[i+1]) === 1) {
+          swap(arr, i, i + 1);
+          didSwap = true;
+        }     
+      }
+      else if (arr[i] > arr[i + 1]) {
         swap(arr, i, i + 1);
         didSwap = true;
       }
     }
     unsortedEnd--;
   }
-
   return arr;
 }
-
 
 function swap(arr, index1, index2) {
   const temp = arr[index1];
   arr[index1] = arr[index2];
   arr[index2] = temp;
 }
+
+function comparator (a, b) {
+  if (a.age > b.age) return -1; // returning `-1` means "a goes before b"
+  if (a.age < b.age) return 1;  // returning  `1` means "b goes before a"
+  return 0; // returning 0 means "a and b are equivalent"
+}
+
+console.log(bubbleSort([{age: 1}, {age:30}, {age:4}], comparator));
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -6,6 +6,7 @@ describe('Bubble Sort', function(){
   beforeEach(function() {
     spyOn(window, 'swap').and.callThrough();
     numArray = [7, 99, 3, 5, 1, 6, 2, 4, 8];
+    anObj = [{age: 1}, {age:30}, {age:4}]
   });
 
   // it('Number of times swap is called to be 3', function() {
@@ -13,6 +14,14 @@ describe('Bubble Sort', function(){
   //   expect(window.swap.calls.count()).toEqual(3)
   
   // });
+  it('properly sorts objects', function() {
+    let sorted = bubbleSort(anObj, function comparator (a, b) {
+      if (a.age < b.age) return -1; // returning `-1` means "a goes before b"
+      if (a.age > b.age) return 1;  // returning  `1` means "b goes before a"
+      return 0; // returning 0 means "a and b are equivalent"
+    });
+    expect(sorted).toEqual([{age:1}, {age:4}, {age:30}])
+  });
 
   it('properly sorts an array of numbers', function() {
     const sortedArray = bubbleSort(numArray);
