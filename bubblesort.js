5d159e4ef5cd71c3d9b5a17f359606eda80649dd
Blake Zeiger <blakezeiger@gmail.com>
2018-03-05 14:49:32 -0500

    updates

--- bubblesort.js
+++ bubblesort.js
@@ -8,7 +8,7 @@ function bubbleSort(arr, func) {
         if(func(arr[i], arr[i+1]) === 1) {
           swap(arr, i, i + 1);
           didSwap = true;
-        }     
+        }  
       }
       else if (arr[i] > arr[i + 1]) {
         swap(arr, i, i + 1);
@@ -19,15 +19,14 @@ function bubbleSort(arr, func) {
   }
   return arr;
 }
-
 function swap(arr, index1, index2) {
   const temp = arr[index1];
   arr[index1] = arr[index2];
   arr[index2] = temp;
 }
-
 function comparator (a, b) {
-  if (a.age > b.age) return -1; // returning `-1` means "a goes before b"
-  if (a.age < b.age) return 1;  // returning  `1` means "b goes before a"
+  if (a.age < b.age) return -1; // returning `-1` means "a goes before b"
+  if (a.age > b.age) return 1;  // returning  `1` means "b goes before a"
   return 0; // returning 0 means "a and b are equivalent"
 }
+
