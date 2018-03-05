d5120aef88509011228e112a82534f13c86abc5f
Ragheed Al-midani <ralmidani@gmail.com>
2018-03-05 12:14:14 -0500

    implement swap and bubble sort

--- bubblesort.js
+++ bubblesort.js
@@ -1,12 +1,23 @@
-let swapCounter = 0
-let comparisonCounter = 0
-
 function bubbleSort(arr) {
+  let didSwap = true;
+  let unsortedEnd = arr.length - 1;
+  while (unsortedEnd !== 0 && didSwap === true) {
+    didSwap = false;
+    for (let i = 0; i < unsortedEnd; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+        didSwap = true;
+      }
+    }
+    unsortedEnd--;
+  }
 
-  return arr
+  return arr;
 }
 
 
 function swap(arr, index1, index2) {
-
+  const temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,24 +1,22 @@
 describe('Bubble Sort', function(){
-
-  const numArray = [3, 2, 1,]
-
-
-  beforeAll(function() {
-    spyOn(window, 'swap').and.callThrough();
-
-
-  });
-
-  it('Number of times swap is called to be 3', function() {
-    bubbleSort(numArray)
-    expect(window.swap.calls.count()).toEqual(3)
+  let numArray;
   
+
+
+  beforeEach(function() {
+    spyOn(window, 'swap').and.callThrough();
+    numArray = [7, 99, 3, 5, 1, 6, 2, 4, 8];
   });
 
-  it('Properly sorts an array of numbers', function() {
-    const sortedArray = bubbleSort(numArray)
-    expect(sortedArray).toEqual([1,2,3])
+  // it('Number of times swap is called to be 3', function() {
+  //   bubbleSort(numArray);
+  //   expect(window.swap.calls.count()).toEqual(3)
+  
+  // });
 
+  it('properly sorts an array of numbers', function() {
+    const sortedArray = bubbleSort(numArray);
+    expect(sortedArray).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 99]);
   });
 
   it('handles an empty array', function(){
